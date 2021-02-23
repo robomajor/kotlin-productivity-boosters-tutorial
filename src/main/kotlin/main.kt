9a2981497dca83c9b6005076c39e681335567b74
robomajor <robokajor@gmail.com>
2021-02-24 00:13:58 +0100

    Added code lines about named arguments

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+
+    println(format("mario", "example.com"))     //Calls a function with argument values.
+    println(format("domain.com", "username"))   //Calls a function with switched arguments. No syntax errors, but the result is incorrect.
+    println(format(userName = "foo", domain = "bar.com"))       //Calls a function with named arguments.
+    println(format(domain = "frog.com", userName = "pepe"))     //When invoking a function with named arguments they can by in any order.
+
 }
+
+fun format(userName: String, domain: String) = "$userName@$domain"
