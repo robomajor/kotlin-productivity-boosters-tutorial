756a7780f487e414bc7ae2a69cf398744c87dfa0
robomajor <robokajor@gmail.com>
2021-02-24 00:15:26 +0100

    Added code lines about String templates

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,6 +5,10 @@ fun main() {
     println(format(userName = "foo", domain = "bar.com"))       //Calls a function with named arguments.
     println(format(domain = "frog.com", userName = "pepe"))     //When invoking a function with named arguments they can by in any order.
 
+    val greeting = "Kotliner"
+
+    println("Hello $greeting")                  //Prints a string with a variable reference. References in strings start with $.
+    println("Hello ${greeting.toUpperCase()}")  //Prints a string with an expression. Expressions start with $ and are enclosed in curly braces.
 }
 
 fun format(userName: String, domain: String) = "$userName@$domain"
